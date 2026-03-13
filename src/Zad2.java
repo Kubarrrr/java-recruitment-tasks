63f5811dbfd733a0ceddc3fde2ac0e326fb6fa81
Kuba <87541618+Kubarrrr@users.noreply.github.com>
2026-03-13 14:20:00 +0100

    Second task done and correct

--- src/Zad2.java
+++ src/Zad2.java
@@ -1,5 +1,33 @@
-public class Zad2 {
-    class marek {
 
+public void findProfit(int[] array) {
+    int maxWorth = 0;
+    int buyDay = 0;
+    int sellDay = 0;
+
+    for (int i = 0; i < array.length; i++) {
+        for (int j = i; j < array.length; j++) {
+            if (array[j] - array[i] > maxWorth) {
+                maxWorth = array[j] - array[i];
+                buyDay = i;
+                sellDay = j;
+            }
+        }
+    }
+
+    if (maxWorth == 0) {
+        System.out.println("-1 Nie można osiągnąć zysku");
+    } else {
+        System.out.println("Zysk: " + maxWorth);
     }
 }
+
+void main() {
+    int[] array = {44, 30, 24, 32, 35, 30, 40, 38, 15};
+    int[] array2 = {10,9,8,2};
+
+    findProfit(array);
+    findProfit(array2);
+}
+
+
+
