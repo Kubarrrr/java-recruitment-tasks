d2888816100dcee72fcc465b4f8ca4b6d8adb160
Kuba <87541618+Kubarrrr@users.noreply.github.com>
2026-03-24 22:56:24 +0100

    Task 7 -> validator to phone number

--- src/Zad7.java
+++ src/Zad7.java
@@ -1,7 +1,22 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Zad7 {
+
+    public static boolean isPhoneNumberValidate(String phoneNumber) {
+        Pattern pattern = Pattern.compile("[0-9]{3}([:,\\- ])[0-9]{3}\\1[0-9]{3}");
+        Matcher matcher = pattern.matcher(phoneNumber);
+
+        return matcher.matches();
+    }
 
 
     public static void main(String[] args) {
+        System.out.println("Number 999-999-123 -> " + isPhoneNumberValidate("999-999-123"));
+        System.out.println("Number 999 9929-123 -> " + isPhoneNumberValidate("999 9929-123"));
+        System.out.println("Number 1999:999-123 -> " + isPhoneNumberValidate("199:999-123"));
+        System.out.println("Number 000 129 123 -> " + isPhoneNumberValidate("000 129 123"));
+        System.out.println("Number 000:129:123 -> " + isPhoneNumberValidate("000:129:123"));
 
     }
 }
